aadc63e61427b7a7867be8e0b82cdc961c519614
efpcode <efpjobhunt@gmail.com>
2025-11-15 11:19:16 +0100

    (refactors) project to be a bit cleaner

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
-from src.logger import DailyEntry
-from collections import namedtuple
-
+from src.entry_logger import WeeklyEntry
+from src.config import WEEKLY_LOG_QUESTIONS
+from src.entry_prompter import question_to_answer
 
 def main():
-    data_point = namedtuple("dailylog", ["header", "rating", "bodytext"])
-    user_input = [
-        input(f"what is today's {i}: ") for i in data_point._fields if i != "bodytext"
-    ]
+    markdown_header = ("header", "rating")
+    user_input = [input(f">>> What is this week's {i}: ") for i in markdown_header]
 
-    bodytext_input = []
+    alltext = [ question_to_answer(q) for q in WEEKLY_LOG_QUESTIONS]
 
-    print("\nEnter your log and to exit finish with empty line.\n")
-    while True:
-        line = input()
-
-        if not line.strip():
-            bodytext_input.append("\n--End--\n")
-            break
-        bodytext_input.append(line)
-
-    bodytext = "\n".join(bodytext_input)
-    entry = DailyEntry(body_text=bodytext, header=user_input[0], rating=user_input[1])
+    entry = WeeklyEntry(body_text=alltext, header=user_input[0], rating=user_input[1])
     entry.save_log()
     print("Daily log saved successfully!")
 
